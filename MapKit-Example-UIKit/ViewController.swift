2f68b45f34258723355cf9b9a7d4905eef3919bc
Matthew Dolan <mdolancode@gmail.com>
2024-07-05 21:32:52 -0700

    Setup location manager, request users location and setup map view.

--- MapKit-Example-UIKit/ViewController.swift
+++ MapKit-Example-UIKit/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Requests users location
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        // Setup Map View
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+            mapView.setRegion(region, animated: true)
+        }
+    }
 }
-
