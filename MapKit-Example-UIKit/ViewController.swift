d39a95c0ba26cafd73e2c73a0da2469fcb6a7c17
Matthew Dolan <mdolancode@gmail.com>
2024-07-05 21:57:59 -0700

    Created customized pin view.

--- MapKit-Example-UIKit/ViewController.swift
+++ MapKit-Example-UIKit/ViewController.swift
@@ -40,3 +40,38 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         }
     }
 }
+
+extension ViewController {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let identifier = "CustomAnnotationView"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
+            
+            // Custom pin image
+            annotationView?.image = UIImage(named: "custom_pin")
+            
+            // Add a detail button
+            let detailButton = UIButton(type: .detailDisclosure)
+            annotationView?.rightCalloutAccessoryView = detailButton
+        } else {
+            annotationView?.annotation = annotation
+        }
+        
+        return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if let annotation = view.annotation as? CustomAnnotation {
+            let alert = UIAlertController(title: annotation.title, message: annotation.subtitle, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+}
