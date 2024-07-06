3958c6063edbaed4377f83debea4beb2c888f227
Matthew Dolan <mdolancode@gmail.com>
2024-07-05 18:05:13 -0700

    Initial Commit

---
+++ MapKit-Example-UIKitUITests/MapKit_Example_UIKitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MapKit_Example_UIKitUITestsLaunchTests.swift
+//  MapKit-Example-UIKitUITests
+//
+//  Created by Matthew Dolan on 2024-07-05.
+//
+
+import XCTest
+
+final class MapKit_Example_UIKitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
